6e1fc515e303d59d0afe9082374d40a538444a81
Phuong Nguyen <63515014+PhuongBK201@users.noreply.github.com>
2023-04-17 16:20:41 +0000

    IAC

--- 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c9-nullresource-provisioners.tf
+++ 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c9-nullresource-provisioners.tf
@@ -7,18 +7,18 @@ resource "null_resource" "name" {
     host     = aws_eip.bastion_eip.public_ip    
     user     = "ec2-user"
     password = ""
-    private_key = file("private-key/terraform-key.pem")
+    private_key = file("private-key/chester-ecv-oregon.pem")
   }  
 
 ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
-    source      = "private-key/terraform-key.pem"
-    destination = "/tmp/terraform-key.pem"
+    source      = "private-key/chester-ecv-oregon.pem"
+    destination = "/tmp/chester-ecv-oregon.pem"
   }
 ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
-      "sudo chmod 400 /tmp/terraform-key.pem"
+      "sudo chmod 400 /tmp/chester-ecv-oregon.pem"
     ]
   }
 }
